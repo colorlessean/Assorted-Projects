975718a82b90684b838590be370910c7bd19b394
Sean O'Sullivan <seanosullivan646@gmail.com>
2023-12-17 15:37:58 -0500

    finalized docker file for apiserver and created docker-compose file; all works

--- go-projects/twitter-clone/backend/apiserver/internal/api/handlers/root_handler.go
+++ go-projects/twitter-clone/backend/apiserver/internal/api/handlers/root_handler.go
@@ -1,7 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	// don't do much here yet
+	body := map[string]string{
+		"message": "successfully reached the web server",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(body)
 }
